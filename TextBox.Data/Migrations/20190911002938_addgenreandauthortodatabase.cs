73e406d5d42f0f0da7d95c627e793a6201183dca
kko3 <kko3@zips.uakron.edu>
2019-09-10 19:30:24 -0500

    author and genre added to a migration

---
+++ TextBox.Data/Migrations/20190911002938_addgenreandauthortodatabase.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TextBox.Data.Migrations
+{
+    public partial class addgenreandauthortodatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BookGenre = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Genres_BookGenre",
+                table: "Genres",
+                column: "BookGenre",
+                unique: true,
+                filter: "[BookGenre] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
--- TextBox.Data/TextBoxDBContext.cs
+++ TextBox.Data/TextBoxDBContext.cs
@@ -5,30 +5,30 @@ namespace TextBox.Data
 {
     public class TextBoxDBContext : DbContext
     {
-      public DbSet<Book> Books { get; set; }
+      //public DbSet<Book> Books { get; set; }
 
       public DbSet<Author> Authors { get; set; }
 
       public DbSet<Genre> Genres { get; set; }
 
-      public DbSet<Review> Reviews { get; set; }
+      //public DbSet<Review> Reviews { get; set; }
 
       protected override void OnConfiguring(DbContextOptionsBuilder builder)
       {
-
+        builder.UseSqlServer("Server=tcp:keilpizza.database.windows.net,1433;Initial Catalog=TextBoxDB;Persist Security Info=False;User ID=sqladmin;Password=Password12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
       }
       
       protected override void OnModelCreating(ModelBuilder builder)
       {
-        builder.Entity<Book>().HasKey(b=>b.Id);
+        //builder.Entity<Book>().HasKey(b=>b.Id);
 
         builder.Entity<Author>().HasKey(a=>a.Id);
 
         builder.Entity<Genre>().HasKey(g=>g.Id);
         builder.Entity<Genre>().HasIndex(g=>g.BookGenre).IsUnique();
 
-        builder.Entity<Review>().HasKey(r=>r.Id);
-        builder.Entity<Review>().HasIndex(r=>r.ReviewWriter).IsUnique();
+        /* builder.Entity<Review>().HasKey(r=>r.Id);
+        builder.Entity<Review>().HasIndex(r=>r.ReviewWriter).IsUnique();*/
       }
     }
 }
