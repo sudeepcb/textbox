fb4c9b21441782f2adbbb49a598558b14c74cea0
sudeep baniya <sudeep.cb@gmail.com>
2019-09-13 15:54:18 -0500

    ok

--- TextBox.MVCClient/ViewModels/BookPageVM.cs
+++ TextBox.MVCClient/ViewModels/BookPageVM.cs
@@ -7,9 +7,10 @@ namespace TextBox.MVCClient.ViewModels
   { 
        public Book book { get; set; }
        public Genre Genre { get; set; }
-       public List<Books> allBooks { get; set; }
+       public List<Book> allBooks { get; set; }
        public List<Genre> allGenres { get; set; }
-
+      
+       public List<Book> certainBooks { get; set; }
        public List<Author> allAuthors { get; set; }
        public List<string> allSeries { get; set; }
 
