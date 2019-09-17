685f2ced1aa69e16bb6ca98f9fb6b7080ef7d8eb
sudeep baniya <sudeep.cb@gmail.com>
2019-09-17 00:01:43 -0500

    ?

--- TextBox.MVCClient/Models/CartModel.cs
+++ TextBox.MVCClient/Models/CartModel.cs
@@ -58,8 +58,10 @@ namespace TextBox.MVCClient.Models
         }
       }
     }
+    
     public void setBookList()
       {
+        booksList = new List<Book>();
         
       foreach (var bO in _db.BooksOnOrder.ToList())
         {
